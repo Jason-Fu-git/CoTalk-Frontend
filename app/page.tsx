d47aa5060f8c832d5fd42f0e27200743211dadcd
Titi <li-gc22@mails.tsinghua.edu.cn>
2024-03-26 09:53:24 +0800

    feat(bootstrap):增加bootstrap和popperjs依赖

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,14 @@ import 'bootstrap/dist/css/bootstrap.css'
 export default function Page() {
     return (
 		<body>
-			<nav class="navbar nacbar-expand-md nacbar-light bg-light mb-4 border">
+			<nav className="navbar nacbar-expand-md nacbar-light bg-light mb-4 border">
 				<Link href="/">
 					CoTalk
 				</Link>
-				<button class="navbar-toggler" type="button" data-toggle="collapse"
+				<button className="navbar-toggler" type="button" data-toggle="collapse"
 					data-target="#navbarCollapse" aria-controls="navbarCollapse"
 					aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span></button>
+					<span className="navbar-toggler-icon"></span></button>
 
 				<ul class="navbar-nav ml-auto" style="flex-direction:row">
 						<li class="nav-item">
@@ -24,12 +24,12 @@ export default function Page() {
 						</li>
 				</ul>
 			</nav>
-			<div class="jumbotron">
-				<h1 class="display-3">Hello,</h1>
+			<div className="jumbotron">
+				<h1 className="display-3">Hello,</h1>
 
-				<p class="lead">Welcome to CoTalk.</p>
+				<p className="lead">Welcome to CoTalk.</p>
 
-				<a class="btn btn-lg btn-primary" href="/user/register"
+				<a className="btn btn-lg btn-primary" href="/user/register"
 					role="button">Register &raquo;</a>
 			</div>
 		</body>
