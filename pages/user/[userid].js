166cb0e3dd8b6af28f33ecaf39979a89ca921f08
Titi <li-gc22@mails.tsinghua.edu.cn>
2024-03-31 14:01:01 +0800

    feat(user): 实现好友列表

--- pages/user/[userid].js
+++ pages/user/[userid].js
@@ -1,16 +1,20 @@
-export async function getServerSideProps({params}) {
-    const {userid}=params;
-    return {
-        props: {
-            userid
-        }
-    }
+import {useEffect} from 'react';
+import Link from 'next/link';
+import axios from 'axios';
+
+export async function getServerSideProps(ctx) {
+    const {userid}=ctx.query;
+    const userReq=await axios.get('https://localhost:8000/api/')
 }
 
-function Greet(props) {
+function Account(props) {
     return (
-        <div>Hello, user#{props.userid}</div>
+        <>
+            <div>
+                User ID: {props.userid}.
+            </div>
+        </>
     )
 }
 
-export default Greet;
+export default Account;
