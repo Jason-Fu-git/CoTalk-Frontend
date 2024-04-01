db7536e0a140cef3003089550e8fa2a4faa0ed56
李谷蝉 <li-gc22@mails.tsinghua.edu.cn>
2024-04-01 15:03:12 +0800

    feat(user): 完成好友列表基本设计

--- pages/user/[userid].js
+++ pages/user/[userid].js
@@ -6,15 +6,47 @@ const BACKEND_URL = "https://cotalkbackend-concord.app.secoder.net";
 export async function getServerSideProps(ctx) {
     const {userid}=ctx.query;
 //    const userReq=await axios.get(`${BACKEND_URL}/api/user/${userid}`);
-    const userReq={
-        "data":{
-            "user_name":"Test",
-            "user_email":"aa@bb.com"
-        }
+    console.log(userid);
+    if (userid == 1)
+    {
+        const userReq={
+            "data":{
+                "user_name":"Test1",
+                "user_email":"test1@xxx.com"
+            }
+        }; 
+        return {
+            props: {
+                user: userReq.data
+            }
+        }     
     }
-    return {
-        props: {
-            user: userReq.data
+    if (userid == 2)
+    {
+        const userReq={
+            "data":{
+                "user_name":"Test2",
+                "user_email":"test2@xxx.com"
+            }
+        };   
+        return {
+            props: {
+                user: userReq.data
+            }
+        }    
+    }
+    else
+    {
+        const userReq={
+            "data":{
+                "user_name":"Test",
+                "user_email":"test@xxx.com"
+            }
+        };  
+        return {
+            props: {
+                user: userReq.data
+            }
         }
     }
 }
