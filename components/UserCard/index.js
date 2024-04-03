d1d78b8eb24042dc7e2605fc327d9ac97942e965
root <2022012200@admin.secoder.net>
2024-04-03 10:01:08 +0800

    fix(frontend-ui):test ci/cd

--- components/UserCard/index.js
+++ components/UserCard/index.js
@@ -4,11 +4,11 @@ function UserCard(props) {
   return (
     <Link href={`/user/${props.user_id}`} passHref>
       <div className="dark:bg-gray-800 bg-gray-100 cursor-pointer dark:text-white p-4 rounded-md text-center shadow-xl">
-        <img
+        {/* <img
           src="https://images.unsplash.com/photo-1605460375648-278bcbd579a6"
           alt={props.user_name}
           className="w-16 bg-gray-400 rounded-full m-auto"
-        />
+        /> */}
         <div className="mt-2 font-bold">
           {props.user_name}
         </div>
