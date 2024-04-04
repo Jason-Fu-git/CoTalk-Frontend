5f15e32e2bf3d972062a24350e8426bce2f924d9
李谷蝉 <li-gc22@mails.tsinghua.edu.cn>
2024-04-04 20:21:39 +0800

    feat(search): 完善搜索用户界面

--- components/TopBar/index.js
+++ components/TopBar/index.js
@@ -1,31 +1,9 @@
 import Link from "next/link";
-import { useTheme } from 'next-themes';
 
 function TopBar() 
 {
-	const { theme, setTheme } = useTheme();
-	const dark = theme === 'dark';
-  
-	const toggleTheme = () => {
-	  setTheme(dark ? 'light' : 'dark');
-	};
-
   	return (
-    <div className="w-full p-2 bg-blue-300 flex space-x-4">
-		<div className="w-10/12 m-auto">
-			<button 
-				onClick={toggleTheme}
-				className="
-				dark:bg-blue-400
-				dark:text-gray-800
-				bg-blue-400
-				text-white
-				font-semibold
-				p-2
-				rounded-md">
-			更改色调
-			</button>
-		</div>
+    <div className="w-full p-2 bg-blue-300 flex auto">
 		<div className="w-10/12 m-auto">
 			<Link href={`/user/self/friends`} passHref>
                 <button className="
--- pages/user/self/friends.js
+++ pages/user/self/friends.js
@@ -1,16 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import UserCard from '../../../components/UserCard';
+import UserCard from '@/components/UserCard';
 import {BACKEND_URL} from '@/app/constants/string';
 import React, { useState,useEffect } from "react";
 import {request} from "@/app/utils/network";
-import { store } from "@/app/redux/store"
+import { store } from "@/app/redux/store";
+import Link from 'next/link';
 
 function Friends() 
 {
     const [friends, setFriends] = useState([]);
-    const [query, setQuery] = useState("");
-    const [searchResult, setSearchResult] = useState([]);
-    const [hasSearched, setHasSearched] = useState(false);
 
     useEffect(() => {
         request(`${BACKEND_URL}/api/user/${store.getState().auth.id}/friends`, "GET", true)
@@ -18,39 +16,12 @@ function Friends()
         setFriends(res.friends);
         });
     }, []);
-    
-    const handleSearch = () => {
-        setHasSearched(true);
-        request(`${BACKEND_URL}/api/user/?search_text=${query}`, "GET", false)
-        .then((res) => {
-            setSearchResult(res.friends);
-        });
-    };
 
     return (
         <>
-            <div>
-            <input
-                type="text"
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-            />
-            <button onClick={handleSearch}>Search</button>
-            {hasSearched && (searchResult.length > 0 ? (
-                searchResult.map((result) => (
-                <div key={result.user_id}>
-                    <p>Name: {result.user_name}</p>
-                    <p>Email: {result.user_email}</p>
-                </div>
-                ))
-            ) : (
-                <p>没有搜索结果</p>
-            ))}
-
-            </div>
             <div className="sm:w-9/12 sm:m-auto pt-16 pb-16">
                 <h1 className="
-                    dark:text-white text-5xl font-bold text-center">
+                    dark:text-white text-4xl font-bold text-center">
                     所有好友
                 </h1>
                 <div className="grid gap-8 grid-cols-1 sm:grid-cols-3 mt-14
@@ -62,6 +33,19 @@ function Friends()
                     ))}
                 </div>
             </div> 
+            <Link href={`/user/search`} passHref>
+                <button className="
+                    dark:bg-blue-400
+                    dark:text-gray-800
+                    bg-blue-400
+                    text-white
+                    font-semibold
+                    p-2
+                    rounded-md
+                    mt-6">
+                  搜索用户
+                </button>
+            </Link>
         </>
     )
 }
