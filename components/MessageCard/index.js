8d412d1993fed058461ec0decf5fa666c9becd8b
root <2022012200@admin.secoder.net>
2024-04-16 08:56:57 +0800

    fix(frontend-ui):fix Websocket

--- components/MessageCard/index.js
+++ components/MessageCard/index.js
@@ -1,8 +1,17 @@
 import { store } from "@/app/redux/store";
 import Image from 'next/image';
-
+import React,{ useState, useEffect } from "react";
+import { BACKEND_URL } from '@/app/constants/string';
+import { request } from "@/app/utils/network";
 function MessageCard(props) 
 {
+	const [avatar, setAvatar] = useState('');
+	useEffect(()=>{
+		request(`${BACKEND_URL}/api/user/private/${props.sender_id}/avatar`, "GET", false)
+		.then((url) => {
+		setAvatar(url);
+		});
+	}, []);
 	const my_id=store.getState().auth.id;
 	if (props.sender_id === my_id)
 	{
@@ -15,12 +24,10 @@ function MessageCard(props)
 						<div class="row g-0">
 							<div class="col-md-4">
 								<Image
-									src={(props.sender_avatar.url) ? 
-									(props.sender_avatar.url):
-									(props.sender_avatar.src)}
+									src={avatar.src?avatar.src:avatar.url}
 									alt={props.sender_name}
-									width={props.sender_avatar.width}
-									height={props.sender_avatar.height}/>
+									width={avatar.width}
+									height={avatar.height}/>
 							</div>
 							<div class="col-md-8">
 								<div class="card-body">
