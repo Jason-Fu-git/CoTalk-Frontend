7cf19592836d34ff1cb4914de93998e971217a8a
李谷蝉 <li-gc22@mails.tsinghua.edu.cn>
2024-04-30 08:17:04 +0800

    feat(chat): 增加历史记录

--- pages/chat/[chatid]/conversation.js
+++ pages/chat/[chatid]/conversation.js
@@ -2,26 +2,83 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React, { useState, useEffect } from 'react';
 import {useRouter} from 'next/router';
 import Link from 'next/link';
+
 import MessageCard from '@/components/MessageCard';
 import { store } from "@/app/redux/store";
 import { request } from "@/app/utils/network";
 import { BACKEND_URL } from '@/app/constants/string';
 
+const [messages, setMessages]=useState([]);
+const [count, setCount]=useState(0);
+
+async function getChatMessages(chatid)
+{
+    const res= await request(`${BACKEND_URL}/api/chat/${chatid}/messages`, "GET", true)
+    if (Number(res.code)===0)
+    {
+        const history=res.messages;
+        let new_list=[];
+
+        for (let item of history) 
+        {
+            setCount(count + 1);
+
+            // Sender avatar
+            let sender_avatar = "";
+            const url = await request(`${BACKEND_URL}/api/user/private/${item.sender_id}/avatar`, "GET", false);
+            sender_avatar = url;
+            const datetime = new Date(item.create_time).toLocaleString('en', dateOptions);
+
+            // Mark as read
+            if (!item.read_users.includes(self_id)) 
+            {
+                await request(`${BACKEND_URL}/api/message/${item.msg_id}/management`, "PUT", true, 
+                {
+                    "user_id": self_id,
+                });
+            }
+
+            new_list.push({
+                index: count, 
+                sender_name: "后端目前没有返回用户名",
+                sender_id: item.sender_id,
+                sender_avatar: sender_avatar,
+                message: item.msg_text,
+                message_id: item.msg_id,
+                datetime: datetime,
+            });
+
+            setCount(count+1);
+        }
+        setMessages(new_list);
+    }
+}
+
 function Conversation()
 {
     const router = useRouter();
     const {chatid} = router.query;
+    const self_id=store.getState().auth.id;
 
-    const url=`wss://cotalkbackend-Concord.app.secoder.net/ws/chat/${chatid}/`;
-    const chatSocket=new WebSocket(url);
-
-    const [messages, setMessages]=useState([]);
-    const [count, setCount]=useState(0);
+    // 第一次渲染时将所有已有消息标记为已读
+    const [firstRender, setFirstRender]=useState(true);
+    // 用这个值来手动刷新
+    const [toggle, setToggle]=useState(true);
 
     useEffect(()=> {
         console.log("useEffect执行刷新");
         console.log("当前消息列表: "+messages);
 
+        if (firstRender)
+        {
+            getChatMessages(chatid);
+            setFirstRender(false);
+        }
+
+        // 连接WebSocket
+        const url=`wss://cotalkbackend-Concord.app.secoder.net/ws/chat/${chatid}/`;
+        const chatSocket=new WebSocket(url);
+    
         const generalUrl="ws://cotalkbackend-Concord.app.secoder.net/ws/main/"+
         store.getState().auth.id+"/"+store.getState().auth.token;
         const generalSocket=new WebSocket(generalUrl);
@@ -30,7 +87,7 @@ function Conversation()
             chatSocket.close();
             generalSocket.close();
         }
-    }, [messages]);
+    }, [toggle]);
 
     //客户端收到消息时触发
     chatSocket.onmessage=function(event) {
@@ -63,6 +120,7 @@ function Conversation()
             
         setCount(count+1);
         setMessages(newMessages);
+        setToggle(!toggle);
     };
 
     chatSocket.onclose=function(event) {
@@ -78,12 +136,27 @@ function Conversation()
         const message=inputArea.value;
         if (message)
         {
+            // 通过WebSocket发送一份
             chatSocket.send(JSON.stringify({
                 'message': message,
                 'sender_id': store.getState().auth.id,
                 'sender_name': store.getState().auth.name,
             }));
-    
+
+            // 通过Http发送一份
+            request(`${BACKEND_URL}/api/message/send`, "POST", true, "application/json", 
+            {
+                "user_id": store.getState().auth.id,
+                "chat_id": chatid,
+                "msg_text": message,
+                "msg_type": "text",
+            })
+            .then((res) => {
+                if (Number(res.code)!==0) {
+                    alert("发送失败");
+                }
+            });
+
             inputArea.value='';
             inputArea.focus();
         }
@@ -96,30 +169,6 @@ function Conversation()
 
     generalSocket.onmessage=function(event) {
         console.log('General websocket receive something');
-        const data=JSON.parse(event.data);
-
-        if (data.type !== 'chat.message')
-        {
-            // 其余内容还未实现
-            console.log('Unknown message');
-            assert(false);
-            return;
-        }
-        if (data.status !== 'send message')
-        {
-            // 撤回还未实现
-            console.log('Unknown status');
-            assert(false);
-            return;
-        }
-
-        const sender_id=data.user_id;
-        const message_id=data.msg_id;
-
-        // 来自其他群聊的新消息还未实现
-        assert(data.chat_id === chatid);
-
-        
     }
 
     generalSocket.onclose=function(event) {
