bc0b0643f4e5de0b1c765b98e7d564ba45467059
田睿 <2022012200@admin.secoder.net>
2024-05-14 04:01:22 +0000

    Revert "Update [chatid].js"
    This reverts commit 3736b28c305228002d9f9ee6cbf4057a313b5744

--- pages/chat/[chatid].js
+++ pages/chat/[chatid].js
@@ -15,11 +15,7 @@ function Chat() {
     const [toggle, setToggle] = useState(true);
     const [notice, setNotice] = useState("没有群公告");
     const [editNotice, setEditNotice] = useState(false);
-    let  chatid = localStorage.getItem("chatid");
-        if (router.query.chatid) {
-            chatid = router.query.chatid;
-            localStorage.setItem("chatid", chatid);
-        }
+    let chatid = 0;
 
     const my_friends = store.getState().auth.friends;
     const my_id = store.getState().auth.id;
@@ -35,7 +31,11 @@ function Chat() {
     };
 
     useEffect(() => {
-
+        chatid = localStorage.getItem("chatid");
+        if (router.query.chatid) {
+            chatid = router.query.chatid;
+            localStorage.setItem("chatid", chatid);
+        }
         request(`${BACKEND_URL}/api/chat/${chatid}/members?user_id=${store.getState().auth.id}`, "GET", true)
             .then((res) => {
                 res.members.forEach(function (element) {
